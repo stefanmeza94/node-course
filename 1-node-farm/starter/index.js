2eb681e72d8c87799f7c259606f9f2d65cf21518
Stefan Meza <stefanmeza93@gmail.com>
2023-02-28 19:44:14 +0100

    Add routing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,24 +1,24 @@
-const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
-// blocking, synchronous way
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-// console.log(textIn);
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
 
-// const textOut = `This is what we know about avocado: ${textIn}\nCreated on ${Date.now()}`;
-
-// fs.writeFileSync('./txt/output.txt', textOut);
-// console.log('File written!');
-
-// non-blocking asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  if (err) return console.log('ERROR 💥');
-
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
-        console.log('Your file has been written');
-      });
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
     });
-  });
+    res.end('<h1>Page NOT Found</h1>');
+  }
 });
-console.log('will read file');
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
+
+// sledeca lekcija: building a very simple API 013
