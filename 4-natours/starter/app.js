f9a049880fa271f05d3dd50edfcb6fed83bf2158
Stefn Meza <stefanmeza93@gmail.com>
2023-07-05 23:46:24 +0200

    Add patch and delete endpoint

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -49,9 +49,72 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = +req.params.id;
+  let updatedTourIndex = tours.findIndex((tour) => tour.id === id);
+
+  if (updatedTourIndex === -1) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  const updatedTour = { ...tours[updatedTourIndex] };
+
+  for (const [key, value] of Object.entries(req.body)) {
+    updatedTour[key] = value;
+  }
+
+  tours[updatedTourIndex] = updatedTour;
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          status: 'error',
+          message: 'An error occured while updating the tour',
+        });
+      }
+      res.status(200).json({
+        status: 'success',
+        message: 'You successfully updated the tour',
+        data: {
+          tour: updatedTour,
+        },
+      });
+    }
+  );
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = +req.params.id;
+  const restOfTheTours = tours.slice().filter((tour) => tour.id !== id);
+
+  if (restOfTheTours.length === tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(restOfTheTours),
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          status: 'error',
+          message: 'An error occured while deleting the tour',
+        });
+      }
+      res.status(200).json({
+        status: 'success',
+        message: 'You successfully deleted tour',
+      });
+    }
+  );
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
 
-// nastavi od 55. Handling PATCH Requests
+// nastavi od 57. Refactoring Our Routes
